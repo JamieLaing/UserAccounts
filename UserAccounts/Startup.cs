c394a953fc7eaeb1687910240d5f7b4b5fa31fff
Jamie Laing <jamie@jamielaing.com>
2019-09-10 11:01:20 -0500

    initial setup

--- UserAccounts/Startup.cs
+++ UserAccounts/Startup.cs
@@ -27,7 +27,7 @@ namespace UserAccounts
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()

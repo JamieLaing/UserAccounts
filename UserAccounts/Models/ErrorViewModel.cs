29b8f30881f27437a9f69ea97778e3e1b7f92cfc
Jamie Laing <jlaing@ravinia.org>
2019-09-10 10:48:35 -0500

    Add project files.

---
+++ UserAccounts/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UserAccounts.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
